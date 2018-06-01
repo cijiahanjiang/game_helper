c961f2406f804b99ba5353dd4dfc52935dca47dc
liujun36 <liujun36@maoyan.com>
2018-06-01 09:22:06 +0800

    修改获取坐标方法

--- src/main/java/edu/bupt/beibei/game_helper/chaojiqunyingzhuan/GetButtonInfo.java
+++ src/main/java/edu/bupt/beibei/game_helper/chaojiqunyingzhuan/GetButtonInfo.java
@@ -1,18 +1,11 @@
 package edu.bupt.beibei.game_helper.chaojiqunyingzhuan;
 
-import edu.bupt.game.helper.sanguo.ButtonLocation;
-
-import java.awt.*;
+import java.awt.MouseInfo;
 import java.util.Scanner;
 
 public class GetButtonInfo {
 
     public static void main(String[] args) {
-        getNativeLocation();
-//        getAbsLocation();
-    }
-
-    public static void getNativeLocation() {
         Scanner sb = new Scanner(System.in);
         int total = 1;
         while (total > 0) {
@@ -20,19 +13,7 @@ public class GetButtonInfo {
             int x = (MouseInfo.getPointerInfo().getLocation().x - ButtonLocation.绝对_左上角.getX()) * 100 / (ButtonLocation.绝对_右下角.getX() - ButtonLocation.绝对_左上角.getX());
             int y = (MouseInfo.getPointerInfo().getLocation().y - ButtonLocation.绝对_左上角.getY()) * 100 / (ButtonLocation.绝对_右下角.getY() - ButtonLocation.绝对_左上角.getY());
             System.out.println("public static Location " + title + " = new Location(" + x + ", " + y + ");");
+            System.out.println(MouseInfo.getPointerInfo().getLocation().x + "," + MouseInfo.getPointerInfo().getLocation().y);
         }
     }
-
-    public static void getAbsLocation() {
-        Scanner sb = new Scanner(System.in);
-        int total = 1;
-        while (total > 0) {
-            String s = sb.nextLine();
-            int x = MouseInfo.getPointerInfo().getLocation().x;
-            int y = MouseInfo.getPointerInfo().getLocation().y;
-            System.out.println(x + "," + y);
-        }
-    }
-
-
 }
