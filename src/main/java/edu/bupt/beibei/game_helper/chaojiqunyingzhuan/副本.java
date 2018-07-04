c00f07850212e0b28cfb42a84ee6167d71985123
cijiahanjiang <liu4876391520@163.com>
2018-07-04 09:08:59 +0800

    提交图片比较代码

--- src/main/java/edu/bupt/beibei/game_helper/chaojiqunyingzhuan/副本.java
+++ src/main/java/edu/bupt/beibei/game_helper/chaojiqunyingzhuan/副本.java
@@ -5,6 +5,7 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 
 public class 副本 {
     private static int instance_available_time = 4;
@@ -26,8 +27,8 @@ public class 副本 {
     public static Location 金满地副本 = new Location(17, 55);
     private static int[] 金满地活动武将数 = {1, 1, 4, 2, 1, 3, 3, 6, 6, 1};
 
-    //    private static Location 副将副本 = new Location(17,55);
-    private static Location 副将副本 = new Location(17, 67);
+    private static Location 副将副本 = new Location(17, 55);
+    //    private static Location 副将副本 = new Location(17, 67);
     private static int[] 副将副本武将数 = {5, 3, 3, 3, 3, 3, 5, 6, 4};
 
     public static Location 技能4 = new Location(60, 79);
@@ -38,11 +39,18 @@ public class 副本 {
     public static Location 默认武将一_左上 = new Location(5, 71);
     public static Location 默认武将一_右下 = new Location(15, 88);
 
+    public static Location 第三位置副本_左上 = new Location(14, 40);
+    public static Location 第三位置副本_右下 = new Location(21, 45);
+
     public static void main(String[] args) throws Exception {
-        int i = 0;
-//        i = instance(i, 副将副本, 副将副本武将数, 29);
-        i = instance(i, 金满地副本, 金满地活动武将数, 39);
-        instance(i, 属性副本, 属性副本武将数);
+        CommonHelper.sleep(2000);
+        getActivityType(第三位置副本_左上, 第三位置副本_右下);
+//        截图保存(第三位置副本_左上, 第三位置副本_右下, "兑换活动");
+//        hasDefaultHero();
+//        int i = 0;
+//        i = instance(i, 副将副本, 副将副本武将数);
+//        i = instance(i, 金满地副本, 金满地活动武将数, 39);
+//        instance(i, 属性副本, 属性副本武将数);
     }
 
     public static int instance(int times, Location location, int[] ints) throws Exception {
@@ -119,14 +127,33 @@ public class 副本 {
         return null;
     }
 
-    private static boolean hasDefaultHero() {
-        Location 左上 = CommonHelper.getAbsLocation(默认武将一_左上);
-        Location 右下 = CommonHelper.getAbsLocation(默认武将一_右下);
+    private static void 截图保存(Location 左上, Location 右下, String name) {
+        左上 = CommonHelper.getAbsLocation(左上);
+        右下 = CommonHelper.getAbsLocation(右下);
         try {
             Robot robot = new Robot();
             Rectangle screenRect = new Rectangle(左上.getX(), 左上.getY(), 右下.getX() - 左上.getX(), 右下.getY() - 左上.getY());
             BufferedImage bfImage = robot.createScreenCapture(screenRect);
-            ImageIO.write(bfImage, "jpg", new File("d:/测试.jpg"));
+            String classPath = 副本.class.getResource("").getPath();
+            String resourcePath = classPath.split("target")[0] + "src/main/resources/" + name + ".jpg";
+            ImageIO.write(bfImage, "jpg", new File(resourcePath));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean hasDefaultHero() {
+        Location 左上 = CommonHelper.getAbsLocation(默认武将一_左上);
+        Location 右下 = CommonHelper.getAbsLocation(默认武将一_右下);
+        try {
+//            BufferedImage bufferedImage = ImageIO.read(new FileInputStream(resourcePath));
+            Robot robot = new Robot();
+            Rectangle screenRect = new Rectangle(左上.getX(), 左上.getY(), 右下.getX() - 左上.getX(), 右下.getY() - 左上.getY());
+            BufferedImage bfImage = robot.createScreenCapture(screenRect);
+//            this.getClass().getResource("").getPath();
+            String classPath = 副本.class.getResource("").getPath();
+            String resourcePath = classPath.split("target")[0] + "src/main/resources/兑换活动.jpg";
+            ImageIO.write(bfImage, "jpg", new File(resourcePath));
             double x = Math.abs(bfImage.getRGB(bfImage.getWidth() / 2, bfImage.getHeight() / 2));
             double y = Math.abs(bfImage.getRGB(0, 0));
             if (x / y > 1.3 || x / y < 0.7) {
@@ -156,5 +183,38 @@ public class 副本 {
         CommonHelper.moveAndClickWithMiddleTerminal(ButtonLocation.相对_具体副本_关闭);
         CommonHelper.moveAndClickWithMiddleTerminal(ButtonLocation.相对_副本_关闭);
         return null;
+    }
+
+    private static int getActivityType(Location 左上, Location 右下) {
+        String classPath = 副本.class.getResource("").getPath();
+        String resourcePath = classPath.split("target")[0] + "src/main/resources/兑换活动.jpg";
+        try {
+            BufferedImage 兑换活动 = ImageIO.read(new FileInputStream(resourcePath));
+            左上 = CommonHelper.getAbsLocation(左上);
+            右下 = CommonHelper.getAbsLocation(右下);
+            Robot robot = new Robot();
+            Rectangle screenRect = new Rectangle(左上.getX(), 左上.getY(), 右下.getX() - 左上.getX(), 右下.getY() - 左上.getY());
+            BufferedImage 当前图片 = robot.createScreenCapture(screenRect);
+            String tmpPath = classPath.split("target")[0] + "src/main/resources/临时文件.jpg";
+            ImageIO.write(当前图片, "jpg", new File(tmpPath));
+            BufferedImage 当前保存图片 = ImageIO.read(new FileInputStream(tmpPath));
+            //比较两个图片的相似度
+            double totalPoint = 0;
+            double same_point = 0;
+            for (int i = 0; i < 兑换活动.getWidth(); i++) {
+                for (int j = 0; j < 兑换活动.getHeight(); j++) {
+                    totalPoint++;
+                    if (兑换活动.getRGB(i, j) == 当前保存图片.getRGB(i, j)) {
+                        same_point++;
+                    }
+                }
+            }
+            if (same_point / totalPoint > 0.8) {
+                return 1;
+            }
+        } catch (Exception e) {
+
+        }
+        return 0;
     }
 }
